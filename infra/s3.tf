c6c1ca3fd9a5fa492be98c24eae0d4814089762a
Dimitris Miaoulis <jimiaoul@gmail.com>
2026-07-02 09:35:15 +0200

    fix: make S3 notification keys unique across buckets
    - added bucket_key to locals for composite key -
    - prevents duplicate key error when multiple buckets have same notification name
    - allows documents and agentcore_runtime buckets both with lambda_events

--- infra/s3.tf
+++ infra/s3.tf
@@ -3,14 +3,15 @@ locals {
     for item in flatten([
       for ps_key, s3 in var.s3s : [
         for as_key, notification in s3.notifications : {
-          key = as_key
+          key         = as_key
+          bucket_key  = ps_key
           value = {
             lambda_function = notification.lambda_function
             bucket          = module.s3[ps_key].s3-id
           }
         }
       ]
-    ]) : item.key => item.value
+    ]) : "${item.bucket_key}-${item.key}" => item.value
   }
 }
 
